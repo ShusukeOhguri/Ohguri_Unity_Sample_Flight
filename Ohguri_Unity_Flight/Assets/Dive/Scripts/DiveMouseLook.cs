585a5b65635c963e22d7b226fc841e551cd0e5c9
Shusuke <oishiinoumiso@gmail.com>
2017-10-12 00:31:37 +0900

    カメラの調整

--- Ohguri_Unity_Flight/Assets/Dive/Scripts/DiveMouseLook.cs
+++ Ohguri_Unity_Flight/Assets/Dive/Scripts/DiveMouseLook.cs
@@ -14,27 +14,27 @@ public class DiveMouseLook : MonoBehaviour {
 	public float minimumY = -60F;
 	public float maximumY = 60F;
 
+	float rotationX = 0F;
 	float rotationY = 0F;
 	bool mouse_on=true;
 
 	void Update ()
 	{	
 		if (mouse_on){
-		
+
 		if (axes == RotationAxes.MouseXAndY)
 		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-			
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;		
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
+			rotationX = Mathf.Clamp (rotationX, minimumX, maximumX);	
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);		
 			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
 		else if (axes == RotationAxes.MouseX)
 		{
 			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
 		}
-		else
+		else if (axes == RotationAxes.MouseY)
 		{
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
@@ -42,6 +42,7 @@ public class DiveMouseLook : MonoBehaviour {
 			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
 		}
 		}
+
 	}
 	
 	void Start ()
---
+++ Ohguri_Unity_Flight/Assets/script/GyroScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroScript : MonoBehaviour {
+
+	#if UNITY_EDITOR
+		private Vector3 rot;
+	#endif
+
+	Quaternion currentGyro;
+
+	void Start(){
+		#if UNITY_EDITOR
+		rot = transform.rotation.eulerAngles;
+		#else
+		Input.gyro.enabled = true;
+		#endif
+	}
+
+	void Update () {
+		#if UNITY_EDITOR
+		float spd = Time.deltaTime*100.0f;
+		if(Input.GetKey(KeyCode.F)){
+			rot.y -= spd;
+		}
+		if(Input.GetKey(KeyCode.H)){
+			rot.y += spd;
+		}
+		if(Input.GetKey(KeyCode.T)){
+			rot.x -= spd;
+		}
+		if(Input.GetKey(KeyCode.G)){
+			rot.x += spd;
+		}
+		transform.rotation = Quaternion.Euler(rot); 
+		#else
+		currentGyro = Input.gyro.attitude;
+		this.transform.localRotation = 
+			Quaternion.Euler(90, 90, 0) * ( new Quaternion (-currentGyro.x, -currentGyro.y, currentGyro.z, currentGyro.w)); 
+		#endif
+	}
+}
