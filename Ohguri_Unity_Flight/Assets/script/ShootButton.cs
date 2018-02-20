d5cd31f45b273a692ea020ba4cb2bd73ba555a9a
Shusuke <oishiinoumiso@gmail.com>
2018-02-20 21:09:13 +0900

    タイトル遷移方法変更

---
+++ Ohguri_Unity_Flight/Assets/script/ShootButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShootButton : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Button ready!");
+	}
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("tap");
+            SceneManager.LoadScene("Main");
+            Debug.Log("ok");
+        }
+    }
+}
+
